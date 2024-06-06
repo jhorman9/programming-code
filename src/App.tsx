78a473a894401a591a3ae72437d4bda2b5d4cbbe
Jhorman Nieto P <jhosmita9@gmail.com>
2024-06-06 13:47:02 -0500

    ajustando los borde de el select

--- src/App.tsx
+++ src/App.tsx
@@ -20,13 +20,13 @@ const App: React.FC = () => {
       <div className='top-container'>
         <h2>Códigos de programación</h2>
         <div>
-          <label htmlFor="language-select">Filtrar por lenguaje: </label>
           <select id="language-select" value={selectedLanguage} onChange={handleLanguageChange}>
             <option value="Todos">Todos</option>
             <option value="PHP">Php</option>
             <option value="JavaScript">JavaScript</option>
             <option value="HTML">Html</option>
-            <option value="CSS">Css</option>
+            <option value="CSS">Css
+            </option>
             {/* Añadir más opciones según sea necesario */}
           </select>
         </div>
