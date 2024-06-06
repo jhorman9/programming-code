c6f866eb995bbf1ed4cb2a3fa8d5b0dd5e2e3b15
Jhorman Nieto P <jhosmita9@gmail.com>
2024-06-06 13:40:27 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import CodeBlock from './components/CodeBlock';
+import codeExamples from './assets/codes.json';
+
+const App: React.FC = () => {
+  const [selectedLanguage, setSelectedLanguage] = useState<string>('Todos');
+
+  const handleLanguageChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedLanguage(event.target.value);
+  };
+
+  const filteredExamples = selectedLanguage === 'Todos'
+    ? codeExamples
+    : codeExamples.filter(example =>
+        example.language.split(', ').map(lang => lang.toLowerCase()).includes(selectedLanguage.toLowerCase())
+      );
+
+  return (
+    <div>
+      <div className='top-container'>
+        <h2>Códigos de programación</h2>
+        <div>
+          <label htmlFor="language-select">Filtrar por lenguaje: </label>
+          <select id="language-select" value={selectedLanguage} onChange={handleLanguageChange}>
+            <option value="Todos">Todos</option>
+            <option value="PHP">Php</option>
+            <option value="JavaScript">JavaScript</option>
+            <option value="HTML">Html</option>
+            <option value="CSS">Css</option>
+            {/* Añadir más opciones según sea necesario */}
+          </select>
+        </div>
+      </div>
+      {filteredExamples.map((example, index) => (
+        <CodeBlock
+          key={index}
+          code={example.code}
+          language={example.language}
+          title={example.title}
+          description={example.description}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/CodeBlock.tsx
@@ -0,0 +1,61 @@
+import React, { useRef } from 'react';
+import Swal from 'sweetalert2';
+
+interface CodeBlockProps {
+  code: string;
+  language?: string;
+  title: string;
+  description?: string;
+}
+
+const CodeBlock: React.FC<CodeBlockProps> = ({ code, language, title, description }) => {
+  const codeRef = useRef<HTMLPreElement>(null);
+
+  const copyToClipboard = () => {
+    if (codeRef.current) {
+      const codeText = codeRef.current.innerText;
+      navigator.clipboard.writeText(codeText).then(() => {
+        const Toast = Swal.mixin({
+          toast: true,
+          position: "top-end",
+          showConfirmButton: false,
+          timer: 3000,
+          timerProgressBar: true,
+          didOpen: (toast) => {
+            toast.onmouseenter = Swal.stopTimer;
+            toast.onmouseleave = Swal.resumeTimer;
+          }
+        });
+        Toast.fire({
+          icon: "success",
+          title: "Código copiado exitosamente",
+        });
+      }).catch(err => {
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: "Algo salió mal!",
+          footer: `<a href="#">${err}</a>`
+        });
+      });
+    }
+  };
+
+  return (
+    <>
+    <section>
+      <h3>{title}</h3>
+      <small>{language}</small>
+      <div className="code-container">
+        <button className="copy-button" onClick={copyToClipboard}>Copiar</button>
+        <pre ref={codeRef}>
+          {code}
+        </pre>
+      </div>
+      <p>{description}</p>
+    </section>
+    </>
+  );
+};
+
+export default CodeBlock;
